4476b4fd8615d2e78c5971ecc16632a48d95a57b
matthieu-gll <matthieu.gelle@gmail.com>
2022-02-21 21:26:52 +0100

    reset front version

--- src/pages/admin/content/index.tsx
+++
@@ -1,9 +0,0 @@
-import type { NextPage } from 'next'
-
-const AdminContent: NextPage = () => {
-  return (
-    <></>
-  )
-}
-
-export default AdminContent
--- src/pages/admin/message/index.tsx
+++
@@ -1,9 +0,0 @@
-import type { NextPage } from 'next'
-
-const AdminMessage: NextPage = () => {
-  return (
-    <></>
-  )
-}
-
-export default AdminMessage
