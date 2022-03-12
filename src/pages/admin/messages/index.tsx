457607197dd7a9256b206f34194f0a95e1b966a0
matthieu-gll <matthieu.gelle@gmail.com>
2022-03-12 18:08:54 +0100

    reset

--- src/pages/admin/messages/index.tsx
+++
@@ -1,9 +0,0 @@
-import type { NextPage } from 'next'
-
-const Messages: NextPage = () => {
-  return (
-    <>messages</>
-  )
-}
-
-export default Messages
--- src/pages/admin/stats/index.tsx
+++
@@ -1,9 +0,0 @@
-import type { NextPage } from 'next'
-
-const Stats: NextPage = () => {
-  return (
-    <>stats</>
-  )
-}
-
-export default Stats
