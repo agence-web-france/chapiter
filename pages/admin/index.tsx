34b850c4815d82d1a5384554d0a2af3e02825442
agence-web-france <mgelle.agencewebfrance@gmail.com>
2022-05-07 09:21:31 +0200

    avancement

---
+++ pages/admin/index.tsx
@@ -0,0 +1,12 @@
+import AdminLayout from 'components/AdminLayout/AdminLayout'
+import type { NextPage } from 'next'
+
+const AdminPage: NextPage = () => {
+  return (
+    <>
+      <AdminLayout />
+    </>
+  )
+}
+
+export default AdminPage
